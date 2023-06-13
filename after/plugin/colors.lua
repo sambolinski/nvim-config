2c29d0ed67f135a95ee36995ff20be32495ca7e9
Sam Hughes <sambolinski@gmail.com>
2023-06-13 23:41:07 +0100

    changes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,7 @@
 #Set the default theme
 vim.cmd.colorscheme('rose-pine-moon')
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none'})
+vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='none'})
+vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='none'})
+
