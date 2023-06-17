dc05d3b73cc7b9aaec66353f78a36ab0ced679c7
Sam Hughes <sambolinski@gmail.com>
2023-06-17 13:53:15 +0100

    replace nerdtree with chadtree

--- after/plugin/nerdtree.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n","<leader>n", ":NERDTreeFocus<CR>")
-vim.keymap.set("n","<C-n>",":NERDTreeToggle<CR>")
--- lua/sambolinski/packer.lua
+++ lua/sambolinski/packer.lua
@@ -46,7 +46,7 @@ return require('packer').startup(function(use)
 		{'L3MON4D3/LuaSnip'},     -- Required
 		}
 	}
-    use('preservim/nerdtree')
+    use('ms-jpq/chadtree')
     use('vim-airline/vim-airline')    
     use('vim-airline/vim-airline-themes')    
     use("eandrju/cellular-automaton.nvim")
